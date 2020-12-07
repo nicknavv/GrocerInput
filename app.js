834058b4ab661cdc1a245fdfa5af428a15b645bf
Nick Navarro <nick.navarro34@gmail.com>
2020-12-07 09:16:27 -0800

    fixing js bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Function to display user feedback 
 
-document.getElementsById("login").onsubmit = function () {
+document.getElementById("login").onsubmit = function () {
     alert("Thanks for submitting! Your results have been emailed to example@gmail.com")
 }
