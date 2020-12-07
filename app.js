b1e7216a816e1bad0d64becf9dabfb2a6f322cfe
Nick Navarro <nick.navarro34@gmail.com>
2020-12-07 09:02:17 -0800

    js function for user feedback

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // Function to display user feedback 
 
+document.getElementsByClassName("form").onsubmit = function () {
+    alert("Thanks for submitting! Your results have been emailed to example@gmail.com")
+}
